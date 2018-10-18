408e905fa56de644607448bd34bd42ebdf5dedbe
theNorthSea <xianggao_1995@163.com>
2018-10-18 21:27:26 +0800

    Site updated: 2018-10-18 21:27:25

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,5 +1,4 @@
 //此函数用于创建复制按钮
-<script type="text/javascript" src="/js/src/clipboard.min.js"></script>
 function createCopyBtns() {
     var $codeArea = $("figure table");
     //查看页面是否具有代码区域，没有代码块则不创建 复制按钮
